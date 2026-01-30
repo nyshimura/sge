af21b263e60f7bec809b53a3e6e45eba35f32e5b
nyshimura <nyshimura@gmail.com>
2026-01-30 17:20:52 -0300

    Refactor student header for improved profile link

--- includes/student_header.php
+++ includes/student_header.php
@@ -48,7 +48,7 @@ if (strpos($userPic, 'data:image') === false && !file_exists($userPic)) {
             
             <div class="user-info">
                 
-                <a href="../profile/index.php" class="user-profile-link" title="Meu Perfil">
+                <a href="../student/profile.php" class="user-profile-link" title="Meu Perfil">
                     <span class="header-user-name">
                         Olá, <strong><?php echo htmlspecialchars(explode(' ', $_SESSION['user_name'])[0]); ?></strong>
                     </span>
@@ -61,4 +61,5 @@ if (strpos($userPic, 'data:image') === false && !file_exists($userPic)) {
             </div>
         </div>
         
+
         <div class="content-wrapper">
